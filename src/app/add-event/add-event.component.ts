86a6565b7def436003cbad9acaf32808023ac19a
Zalendil <noctis_59@yahoo.fr>
2020-09-22 14:53:58 +0200

    Correction bug formulaire error + amélioration script

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -11,6 +11,9 @@ export class AddEventComponent implements OnInit {
   id : number;
   title: string;
   date: string;
+  titleIsValid : boolean;
+  dateIsValid : boolean;
+  isValid : boolean = false;
 
   constructor(private api: ApiService) {
   }
@@ -21,30 +24,46 @@ export class AddEventComponent implements OnInit {
       this.id = Event.id;
       this.title = Event.title;
       this.date = Event.start;
-      console.log(this.id);
     })
   }
 
   valider = () => {
-    console.log(this.id);
-    if(this.id == null){
-      if (this.title != null && this.date != null) {
-        this.api.postApi('/AddDates', { Title: this.title, start: this.date }).subscribe((res: any) => { })
-        alert("L'évenement à bien été ajouté.");
-        this.id = null;
-        this.title = "";
-        this.date = "";
-        this.api.ObservableList.next();
+    this.isValid = true;
+    if(this.CheckFieldIsvalid()){
+      if(this.id == null){
+          this.api.postApi('/AddDates', { Title: this.title, start: this.date }).subscribe((res: any) => { })
+          alert("L'évenement à bien été ajouté.");
+      } else {
+          this.api.put('/EditDate/' + this.id, {Id : this.id, Title: this.title, start: this.date }).subscribe((res: any) => { })
+          alert("L'évenement à bien été modifié.");
       }
-    } else {
-      if (this.title != null && this.date != null) {
-        this.api.put('/EditDate/' + this.id, {Id : this.id, Title: this.title, start: this.date }).subscribe((res: any) => { })
-        alert("L'évenement à bien été modifié.");
-        this.id = null;
-        this.title = "";
-        this.date = "";
-        this.api.ObservableList.next();
-      }
+      this.InitFieldForm();   
+      this.api.ObservableList.next();
+    }    
+  }
+
+  InitFieldForm = () => {
+    this.id = null;
+    this.title = "";
+    this.date = undefined;
+  }
+
+  CheckFieldIsvalid = () => {
+    this.titleIsValid = false;
+    this.dateIsValid = false;
+    
+    if(this.title != "" && this.title != undefined){
+      this.titleIsValid = true;
+    }
+
+    if(this.date != "" && this.date != undefined) {
+      this.dateIsValid = true;
+    }
+
+    if(this.titleIsValid && this.dateIsValid){
+      return true;
+    } else{
+      return false;
     }
   }
 }

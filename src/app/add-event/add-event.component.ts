f9c9eda171d5212beb400e29073814766abdd7eb
Zalendil <noctis_59@yahoo.fr>
2020-10-05 14:41:42 +0200

    ajout date fin à la liste

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -8,20 +8,20 @@ import { AlertService } from '../alert/alert.service';
   styleUrls: ['./add-event.component.css']
 })
 export class AddEventComponent implements OnInit {
-  id : number;
+  id: number;
   title: string;
   StartDate: string;
   EndDate: string;
-  titleIsValid : boolean;
-  dateIsValid : boolean;
-  isValid : boolean = false;
+  titleIsValid: boolean;
+  dateIsValid: boolean;
+  isValid: boolean = false;
 
-  constructor(private api: ApiService, protected AlertService : AlertService) {
+  constructor(private api: ApiService, protected AlertService: AlertService) {
   }
 
 
   ngOnInit() {
-    this.api.ObservableEditEvent.subscribe((Event)=>{
+    this.api.ObservableEditEvent.subscribe((Event) => {
       this.id = Event.id;
       this.title = Event.title;
       this.StartDate = Event.start;
@@ -31,18 +31,18 @@ export class AddEventComponent implements OnInit {
 
   valider = () => {
     this.isValid = true;
-    if(this.CheckFieldIsvalid()){
-      if(this.id == null){
-          this.api.postApi('/AddDates', { Title: this.title, start: this.StartDate, end: this.EndDate }).subscribe((res: any) => { })
-          this.AlertService.success("L'évenement à bien été ajouté.");
-          this.api.ObservableList.next();
+    if (this.CheckFieldIsvalid()) {
+      if (this.id == null) {
+        this.api.postApi('/AddDates', { Title: this.title, start: this.StartDate, end: this.EndDate }).subscribe((res: any) => { })
+        this.AlertService.success("L'évenement à bien été ajouté.");
       } else {
-          this.api.put('/EditDate/' + this.id, {Id : this.id, Title: this.title, start: this.StartDate, end: this.EndDate }).subscribe((res: any) => { })
-          this.AlertService.success("L'évenement à bien été modifié.");
-          this.api.ObservableList.next();
+        this.api.put('/EditDate/' + this.id, { Id: this.id, Title: this.title, start: this.StartDate, end: this.EndDate }).subscribe((res: any) => { })
+        this.AlertService.success("L'évenement à bien été modifié.");
       }
-      this.InitFieldForm();   
-    }    
+      
+      this.InitFieldForm();
+      this.api.ObservableList.next();
+    }
   }
 
   InitFieldForm = () => {
@@ -55,20 +55,20 @@ export class AddEventComponent implements OnInit {
   CheckFieldIsvalid = () => {
     this.titleIsValid = false;
     this.dateIsValid = false;
-    
-    if(this.title != "" && this.title != undefined){
+
+    if (this.title != "" && this.title != undefined) {
       this.titleIsValid = true;
     }
 
-    if(this.StartDate != "" && this.StartDate != undefined) {
+    if (this.StartDate != "" && this.StartDate != undefined) {
       this.dateIsValid = true;
     }
 
-    if(this.EndDate == "" || this.EndDate == undefined){
+    if (this.EndDate == "" || this.EndDate == undefined) {
       this.EndDate = this.StartDate;
     }
 
-    if(this.titleIsValid && this.dateIsValid){
+    if (this.titleIsValid && this.dateIsValid) {
       return true;
     } else {
       this.AlertService.fail("Merci de remplir tous les champs");
